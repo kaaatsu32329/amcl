de04266cad46053abc0a99157ecdde295cc6b046
katsu <katsu1112uchi@gmail.com>
2023-03-16 15:41:03 +0900

    [WIP]

--- particle-filter/src/particle_filter.rs
+++ particle-filter/src/particle_filter.rs
@@ -4,7 +4,7 @@ pub trait PartcleFilter<F>
 where
     F: Float + Zero,
 {
-    fn resampling(&self);
+    fn resampling(&self, weights: &[f64]);
     fn set_likelihood(&mut self, observed: &[F]);
     fn get_filtered(&self) -> F;
 }
--- particle-filter/src/single_point.rs
+++ particle-filter/src/single_point.rs
@@ -1,5 +1,7 @@
 use crate::particle_filter::PartcleFilter;
 
+use rand::Rng;
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct SinglePoint {
     y: f64,
@@ -7,12 +9,23 @@ pub struct SinglePoint {
     sigma: f64,
     alpha: f64,
     likelihood: Vec<f64>,
+    resampled: Vec<f64>,
     filtered: f64,
 }
 
 impl PartcleFilter<f64> for SinglePoint {
-    fn resampling(&self) {
-        todo!()
+    fn resampling(&mut self, weights: &[f64]) {
+        if weights.len() == self.particle {
+            self.resampled.clear();
+
+            let mut rng = rand::thread_rng();
+            let sum_weight = weights.iter().sum::<f64>();
+            let sample_width = sum_weight / self.particle as f64;
+            let start_point = rng.gen_range(0f64..sample_width);
+
+            for i in 0..self.particle {
+            }
+        }
     }
 
     fn set_likelihood(&mut self, observed: &[f64]) {
